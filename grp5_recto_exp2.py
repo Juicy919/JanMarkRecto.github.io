0d822515775c6e91da9a064c8e4b5c82a49e2ce9
Juicy919 <144079521+Juicy919@users.noreply.github.com>
2024-08-31 19:50:10 +0800

    Update

---
+++ grp5_recto_exp2.py
@@ -0,0 +1,66 @@
+num1 = 30
+num2 = 10
+
+result_add = num1 + num2
+result_sub = num1 - num2
+result_mul = num1 * num2
+result_div = num1 / num2
+result_mod = num1 % num2
+result_exp = num1 ** num2
+
+print("Addition: ", result_add)
+print("Subtraction: ", result_sub)
+print("Multiplication: ", result_mul)
+print("Division: ", result_div)
+print("Modulus: ", result_mod)
+print("Exponent: ", result_exp)
+
+num1 = 18.21
+num2 = 17.5
+
+result_add = num1 + num2
+result_sub = num1 - num2
+result_mul = num1 * num2
+result_div = num1 / num2
+result_mod = num1 % num2
+result_exp = num1 ** num2
+
+print("\nAddition (float): ", result_add)
+print("Subtraction (float): ", result_sub)
+print("Multiplication (float): ", result_mul)
+print("Division (float): ", result_div)
+print("Modulus (float): ", result_mod)
+print("Exponent (float): ", result_exp)
+
+num1 = 3 + 4
+num2 = 2 + 5
+result_add = num1 + num2
+result_sub = num1 - num2
+result_mul = num1 * num2
+result_div = num1 / num2
+result_mod = abs(num1 - num2)  
+result_exp = num1 ** 0.5  
+print("\nAddition (complex): ", result_add)
+print("Subtraction (complex): ", result_sub)
+print("Multiplication (complex): ", result_mul)
+print("Division (complex): ", result_div)
+print("Modulus (complex): ", result_mod)
+print("Exponent (complex): ", result_exp)
+
+
+#2
+num1 = 25_000_000 
+num2 = 25000000    
+
+print(num1)
+print(num2)
+
+#3 
+
+int_var = 150
+float_var = 74.25
+complex_var = 17 + 20
+
+print(f"Type of int_var: {type(int_var)}")
+print(f"Type of float_var: {type(float_var)}")
+print(f"Type of complex_var: {type(complex_var)}")
