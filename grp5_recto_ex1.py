3b387ede9e59aa26b78fe87c6588971d23b88390
Juicy919 <144079521+Juicy919@users.noreply.github.com>
2024-08-31 18:49:28 +0800

    Update

---
+++ grp5_recto_ex1.py
@@ -0,0 +1,25 @@
+#My First Simple Script
+
+print("Hello, I'm Jan Mark Recto! from BSCS-3G\n")
+
+age = 22
+course = "Computer Science"
+
+print(age)
+print(course)
+
+
+Neil = 23
+Kyle = 21
+Ralph = 25
+Marry = 40
+
+
+print(Neil)
+print(Kyle)
+print (Ralph)
+print(Marry)
+
+Cute , Pretty , Smile = "Haggard" , "Ugly" , "Sad"
+
+print(Cute , Pretty , Smile)
